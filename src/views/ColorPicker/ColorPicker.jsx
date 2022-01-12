ba5daeb72b403200e5b64828756f089fdac44839
Zack Lyon <zack.j.lyon@gmail.com>
2022-01-12 12:38:35 -0800

    Refactor affirmation state into custom hook

--- src/views/ColorPicker/ColorPicker.jsx
+++ src/views/ColorPicker/ColorPicker.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Display from '../../components/Display/Display';
+import useAffirmation from '../../hooks/useAffirmation.js';
 import useColorPicker from '../../hooks/useColorPicker.js';
 import styles from './ColorPicker.css';
 
@@ -7,20 +8,7 @@ export default function ColorPicker() {
   const [{ fgColor, bgColor, content, didChangeColor }, handleChange] =
     useColorPicker({});
 
-  const [affirmation, setAffirmation] = useState('');
-
-  useEffect(() => {
-    const affirmations = [
-      'Great choice!',
-      'I love that color!',
-      'Looks good!',
-      'What a great color combo!',
-      'Ooh la la, so fancy',
-    ];
-    // Generate a random whole number between 0 and the last index of the array
-    const randomIndex = Math.floor(Math.random() * affirmations.length);
-    setAffirmation(affirmations[randomIndex]);
-  }, [bgColor, fgColor]);
+  const affirmation = useAffirmation({ bgColor, fgColor });
 
   return (
     <>
